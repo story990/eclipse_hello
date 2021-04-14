550d64f0d0430499607cd5df99b4cc8ed44e18e3
story990 <story990@naver.com>
2021-04-14 20:13:17 +0900

    added 'her World'

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println("Hello his World");
+		System.out.println("Hello her World");
 	}
 
 }
