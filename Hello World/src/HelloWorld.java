960be7a36b3b53bfb1ca2545e288bc3226910518
story990 <story990@naver.com>
2021-04-14 20:10:11 +0900

    add print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello his World");
 
 	}
 
